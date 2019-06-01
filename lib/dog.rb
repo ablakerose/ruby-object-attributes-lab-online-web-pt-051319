00fd370e9748cd983314a1e05c67a784f2d5bf96
Blake Rose <bdecktor@gmail.com>
2019-06-01 19:17:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,11 @@
 class Dog
+  attr_accessor :name, :breed
+
+  
   def initialize(name, breed)
       @name = name
       @breed = breed
     end
 
-  def name=(dog_name)
-    @name = dog_name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end
-
-  def breed
-    @breed
-  end
+  
 end
